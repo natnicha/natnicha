f52d9cf310786154bc4902f465223f8143b49d82
Natnicha <nat.rodtong@gmail.com>
2025-09-11 16:38:15 +0200

    feat: grab year of exp by regex

--- scripts/update_exp_year.py
+++ scripts/update_exp_year.py
@@ -1,15 +1,18 @@
+import re
 from datetime import datetime
 
 current_year = datetime.now().year
 value = current_year - 2018
 
 readme_file = "README.md"
-with open(readme_file, "r") as f:
+with open(readme_file, "r", encoding="utf8") as f:
     content = f.read()
 
 new_string = "{0}+ years of experience".format(str(value))
-content = content.replace("6+ years of experience", new_string)
+pattern = "\d*\+ years of experience"
+x = re.search(pattern, content)
 
-with open(readme_file, "w") as f:
+content = content.replace(x.group(), new_string)
+
+with open(readme_file, "w", encoding="utf8") as f:
     f.write(content)
-
